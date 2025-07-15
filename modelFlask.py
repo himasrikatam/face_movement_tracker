bbe612b332286871315a2716c57162e532fbffc2
Himasri <himasrikatam@gmail.com>
2025-07-15 15:59:35 +0530

    added port

--- modelFlask.py
+++ modelFlask.py
@@ -64,4 +64,5 @@ def start_tracker():
     return render_template('home.html', username=session['username'], message="Face tracker started. Press 'esc' to stop.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
